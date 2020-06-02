7bc389a38f4234ea6e21d718d7dfed29a4d55162
FabLeo451 <fab.leo@gmail.com>
2020-06-02 15:54:24 +0200

    docs

--- data/asset/nodes/Standard/ForLoop.java
+++ data/asset/nodes/Standard/ForLoop.java
@@ -3,7 +3,7 @@ int _start_{node.id} = in{1};
 int _end_{node.id} = in{2};
 int _delta_{node.id} = in{1} <= in{2} ? 1 : -1;
 
-for (out{1} = _start_{node.id}; (_start_{node.id} <= _end_{node.id} ? out{1} <= _end_{node.id} : out{1} >= _end_{node.id}) && !in{3}; out{1} += _delta_{node.id}) {
+for (out{1} = _start_{node.id}; (_start_{node.id} <= _end_{node.id} ? out{1} <= _end_{node.id} : out{1} >= _end_{node.id}); out{1} += _delta_{node.id}) {
   exec{0}
 }
 
