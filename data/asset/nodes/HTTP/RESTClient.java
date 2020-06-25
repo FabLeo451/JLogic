48d365935b5a1c71d41375631b5e320227f98c32
FabLeo451 <fab.leo@gmail.com>
2020-06-25 16:19:29 +0200

    REST Client

---
+++ data/asset/nodes/HTTP/RESTClient.java
@@ -0,0 +1,70 @@
+
+boolean _{node.id}_restOK = false;
+
+try {
+  URL _{node.id}_url = new URL(in{2});
+  HttpURLConnection _{node.id}_con = (HttpURLConnection) _{node.id}_url.openConnection();
+
+  String method = "";
+
+  switch (in{1}) {
+    case 0:
+      method = "GET";
+      break;
+
+    case 1:
+      method = "POST";
+      break;
+
+    case 2:
+      method = "PUT";
+      break;
+
+    case 3:
+      method = "DELETE";
+      break;
+
+    default:
+      break;
+  }
+
+  _{node.id}_con.setRequestMethod(method);
+  //_{node.id}_con.setRequestProperty("Ocp-Apim-Subscription-Key", key);
+  _{node.id}_con.setRequestProperty("Content-Type", "application/json");
+  _{node.id}_con.setRequestProperty("Accept", "application/json");
+  HttpURLConnection.setFollowRedirects(true);
+  _{node.id}_con.setInstanceFollowRedirects(false);
+  _{node.id}_con.setDoOutput(true);
+
+  // Send data
+  if (_{node.id}_in[3] != null) {
+    OutputStream _{node.id}_out = _{node.id}_con.getOutputStream();
+    _{node.id}_out.write(((JSONObject)_{node.id}_in[3]).toString().getBytes());
+  }
+
+  InputStream _{node.id}_ip = _{node.id}_con.getInputStream();
+  BufferedReader _{node.id}_br1 = new BufferedReader(new InputStreamReader(_{node.id}_ip));
+
+  out{1} = _{node.id}_con.getResponseCode();
+
+  //System.out.println("Response Message:" + con.getResponseMessage());
+
+  StringBuilder _{node.id}_response = new StringBuilder();
+  String _{node.id}_responseSingle = null;
+
+  while ((_{node.id}_responseSingle = _{node.id}_br1.readLine()) != null) {
+    _{node.id}_response.append(_{node.id}_responseSingle);
+  }
+
+  out{2} = _{node.id}_response.toString();
+  _{node.id}_restOK = true;
+
+} catch (Exception e) {
+    out{4} = e.getMessage();
+}
+
+if (_{node.id}_restOK) {
+  exec{0}
+} else {
+  exec{3}
+}
