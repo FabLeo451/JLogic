d19da95d9b183455fd46e24c71b89b7e30007b4e
FabLeo451 <fab.leo@gmail.com>
2020-07-31 09:55:17 +0200

    Elasticsearch Query

--- plugin/ElasticsearchClient/Query.java
+++ plugin/ElasticsearchClient/Query.java
@@ -1,5 +1,18 @@
 
-out{1} = in{1}.queryToJSON(QueryBuilders.matchAllQuery(), in{2});
+
+switch (in{3}) {
+  case 1:
+    out{1} = in{1}.queryToJSON(QueryBuilders.termQuery(in{4}, in{5}), in{2});
+    break;
+
+  case 2:
+    out{1} = in{1}.queryToJSON(QueryBuilders.matchQuery(in{4}, in{5}), in{2});
+    break;
+
+  default:
+    out{1} = in{1}.queryToJSON(QueryBuilders.matchAllQuery(), in{2});
+    break;
+}
 
 if (out{1} != null) {
   exec{0}
