851e8cad638a97ecc58c100bfcec49be45c65c8f
FabLeo451 <fab.leo@gmail.com>
2020-08-07 19:08:39 +0200

    Fixed edit property

--- src/main/resources/static/js/properties.js
+++ src/main/resources/static/js/properties.js
@@ -7,15 +7,15 @@ function callbackSaveData(dialog) {
   var key = document.getElementById("dataKey").value;
   var type = Number(document.getElementById("dataType").value);
   var dataResponseDiv = document.getElementById("dataResponse");
-  
+
   /*console.log ('key = "'+key+'"');
   //console.log ('type = "'+type+'"');*/
-  
+
   if (key === null || key === "" || key === 'undefined') {
     dialog.showError ('A key must be specified');
     return;
   }
- /* 
+ /*
   if (dialog.isTimestamp()) {
     if (!document.getElementById("date").value) {
       dialog.showError ('Missing date');
@@ -26,13 +26,13 @@ function callbackSaveData(dialog) {
       return;
     }
   }*/
-  
+
   key = key.trim();
 
   if (dialog.checkData()) {
   /*
     var value_str, ts;
-    
+
     switch (type) {
       case 'Integer':
       case 'Double':
@@ -43,25 +43,25 @@ function callbackSaveData(dialog) {
       case 'String':
         value_str = "'"+dialog.getData().replace (/"/g,'\\"')+"'";
         break;
-        
+
       default:
         value_str = JSON.stringify (dialog.getData());
         break;
     }
-    
+
     ts = dialog.isTimestamp() ? ', "is_timestamp": true' : '';
-    
+
     jdata = '{ "'+key+'": { "type": '+type+', "value": '+value_str+ts+'} }';
     */
-    
+
     //var value_str = "'"+dialog.getData().replace (/"/g,'\\"')+"'";
     var jtmp = { "___value___":dialog.getData() };
     jdata = JSON.stringify(jtmp).replace("___value___", key);
-    
+
     console.log (jdata);
-    
+
     var selfDialog = dialog;
-    
+
     callServer ("POST", __request, jdata, function (xhttp) {
         if (xhttp.readyState == 4) {
           if (xhttp.status == 200) {
@@ -77,28 +77,28 @@ function callbackSaveData(dialog) {
     );
   }
   else
-    dialog.showError('Invalid data');  
+    dialog.showError('Invalid data');
 }
 
 function editData (type, key) {
   var flags, value = jsonResponse[key];//.value;
-  
+
   flags = DataFlags.EDIT_ONLY_VALUE;
-  
+
   if (jsonResponse[key].is_timestamp === true)
     flags |= DataFlags.TIMESTAMP;
-  
+
   var dialog = new DialogData ();
-  
+
   dialog.callbackOK = callbackSaveData;
-  
+
   dialog.callbackCancel = function (dialog) { dialog.destroy(); };
   dialog.create(type, key, value, flags);
 }
 
 function selectAll (ev) {
   var selected = ev.target.checked;
-  
+
   for (var i=0; i<nItems; i++)
     document.getElementById("check_"+i).checked = selected;
 }
@@ -130,9 +130,9 @@ function readFile(e) {
     /* Reset so 'changed' event triggers again */
     document.getElementById('file-dialog').value="";
   };
-  
+
   console.log ("Reading file...");
-  
+
   reader.readAsText(file);
 }
 
@@ -147,7 +147,7 @@ function exportJson (jdata) {
       anchor = document.createElement('a');
 
   console.log ("Exporting...");
-  
+
   anchor.download = "data.json";
   anchor.href = (window.webkitURL || window.URL).createObjectURL(blob);
   anchor.dataset.downloadurl = ['text/plain', anchor.download, anchor.href].join(':');
@@ -156,31 +156,31 @@ function exportJson (jdata) {
 
 function exportData () {
   var proceed=false, nSelected=0, jdata = {};
-  
+
   if (nItems == 0)
     return;
-    
+
   for (var i=0; i<nItems; i++) {
     if (document.getElementById("check_"+i).checked) {
       var key = document.getElementById("check_"+i).name;
       var jitem = jsonResponse[key];
-      
+
       jdata[key] = jitem;
-      
+
       nSelected ++;
     }
   }
-  
+
   if (nSelected == 0) {
     //dialogOKCancel ("Export all data?", function (dialog) {
     dialogMessage ("Confirm", "Export all data?", DialogButtons.YES_NO, DialogIcon.QUESTION, function (dialog) {
         dialog.destroy();
         jdata = jsonResponse;
-        
+
         // This function is asyncronous, call function here.
         exportJson (jdata);
       }
-    );  
+    );
   }
   else
     proceed = true;
@@ -194,11 +194,11 @@ function refreshDataCallback (xhttp) {
   //console.log ("refreshDataCallback() readyState = "+xhttp.readyState);
 
     if (xhttp.readyState == 4) { // Request finished and response is ready
-    
+
       //console.log ("refreshDataCallback() status = "+xhttp.status);
-      
+
       if (xhttp.status == 200) {
-      
+
         //document.getElementById("data").innerHTML = "";
         var table = document.getElementById("myTable");
         table.innerHTML = "";
@@ -212,11 +212,11 @@ function refreshDataCallback (xhttp) {
         */
 
         var data = xhttp.responseText;
-        
+
         console.log (data);
-        
+
         jsonResponse = JSON.parse(data);
-        
+
         /* Order keys */
         const ordered = {};
         Object.keys(jsonResponse).sort().forEach(function(key) {
@@ -224,12 +224,12 @@ function refreshDataCallback (xhttp) {
         });
 
         jsonResponse = ordered;
-        
+
         var header = ['<input type="checkbox" onchange="selectAll(event)"> Key', 'Value'];
         //var fields = ['pid', 'ip'];
-        
+
         var tr = document.createElement('tr');
-     
+
         for (var i = 0; i < header.length; i++) {
           var th = document.createElement('th');
           th.classList.add('th1');
@@ -238,40 +238,40 @@ function refreshDataCallback (xhttp) {
           th.innerHTML = header[i];
           tr.appendChild(th);
         }
-        
+
         table.appendChild(tr);
-        
+
         var td, tr, key, value, type, type_str, attr_str='', i=0, /*rowId,*/ valueId, buttonsId;
-        
+
         //console.log (data);
 
           for (key in jsonResponse) {
             /*type = jsonResponse[key].type;
             value = jsonResponse[key].value;*/
-            
-              
+
+
             if (key.toLowerCase().indexOf("password") > -1)
               value = '<span style="color:grey;">hidden</span>';
             else
               value = jsonResponse[key];
-            
+
             //rowId = 'tr'+i;
             /*
             if (jsonResponse[key].is_timestamp === true) {
               value = secondsToString(value);
               //attr_str = '<i class="icon i-clock w3-text-orange" title="Timestamp"></i>';
             }*/
-            
+
             tr = document.createElement('tr');
             tr.classList.add ('tr1');
-            
+
             //for (var i = 0; i < fields.length; i++) {
               /*
               switch (type) {
                 case 'Integer':
                   type_str = jsonResponse[key].is_timestamp ? 'Timestamp' : 'Integer';
                   break;
-                  
+
                 case 'Double':
                   type_str = 'Float';
                   break;
@@ -283,7 +283,7 @@ function refreshDataCallback (xhttp) {
                 case 'String':
                   type_str = 'String';
                   break;
-                  
+
                 default:
                   type_str = 'Json';
                   break;
@@ -296,7 +296,7 @@ function refreshDataCallback (xhttp) {
               td.style.width = "25%";
               td.innerHTML = '<input id="check_'+i+'" type="checkbox" name="'+key+'"> '+key;
               tr.appendChild(td);
-              
+
               // Type
               /*
               td = document.createElement('td');
@@ -304,7 +304,7 @@ function refreshDataCallback (xhttp) {
               td.classList.add ('td1');
               td.classList.add ('td1-border');
               tr.appendChild(td);*/
-              
+
               // Attributes
               /*
               td = document.createElement('td');
@@ -313,16 +313,16 @@ function refreshDataCallback (xhttp) {
               td.classList.add ('td1-border');
               tr.appendChild(td);
               */
-              
+
               // Data
               td = document.createElement('td');
               td.classList.add('td1');
               td.classList.add ('td1-border');
-              td.classList.add('data');              
-              td.innerHTML = `<div class="w3-hover-text-green" onclick='editData (`+BPTypeID.STRING+`, "`+key+`")' style="cursor:pointer;" title="Edit value">`+(typeof value === 'object' ? JSON.stringify(value, null, 2) : value)+`</div>`;
-                
+              td.classList.add('data');
+              td.innerHTML = `<div class="w3-hover-text-green" onclick='editData ("String", "`+key+`")' style="cursor:pointer;" title="Edit value">`+(typeof value === 'object' ? JSON.stringify(value, null, 2) : value)+`</div>`;
+
               tr.appendChild(td);
-              
+
               // Delete
               /*
               td = document.createElement ('td');
@@ -330,32 +330,32 @@ function refreshDataCallback (xhttp) {
               td.innerHTML = '<i class="icon i-trash-alt w3-text-gray w3-hover-text-red" onclick="deleteData (\''+key+'\')" style="cursor:pointer;" title="Delete '+key+'"></i>';
               tr.appendChild (td);
               */
-              
+
               //console.log (typeof value);
 
             table.appendChild(tr);
-            
+
             i ++;
           }
 
         nItems = i;
-        
+
         //document.getElementById('data').appendChild(table);
       }
       else {
-        
+
         //document.getElementById('data').innerHTML = 'Error '+xhttp.status+' '+xhttp.statusText;
       }
     }
-    else if (xhttp.readyState == 3) { // Processing request 
+    else if (xhttp.readyState == 3) { // Processing request
       //document.getElementById('data').innerHTML = "Getting blueprints...";
     }
     else {
       //document.getElementById('data').innerHTML = xhttp.statusText;
     }
-    
+
     //document.getElementById("refresh").disabled = false;
-  
+
   //document.getElementById("refresh").disabled = true;
 }
 
@@ -366,9 +366,9 @@ function refreshData () {
 
 function deleteDataCallback (xhttp) {
   if (xhttp.readyState == 4) { // Request finished and response is ready
-  
+
     //console.log (xhttp.responseText);
-    
+
     if (xhttp.status == 200) {
       showSnacknar("Data successfully deleted", 2000);
       refreshData ();
@@ -377,7 +377,7 @@ function deleteDataCallback (xhttp) {
       //blueprint.getElement().innerHTML = xhttp.statusText;
     }
   }
-  else if (xhttp.readyState == 3) { // Processing request 
+  else if (xhttp.readyState == 3) { // Processing request
     //blueprint.getElement().innerHTML = "Deleting blueprint...";
   }
   else {
@@ -392,19 +392,17 @@ function deleteData (jlist) {
 
 function deleteSelected () {
   var jlist = [], nSelected = 0;
-  
+
   for (var i=0; i<nItems; i++) {
     if (document.getElementById("check_"+i).checked) {
       var key = document.getElementById("check_"+i).name;
-      
+
       jlist.push(key);
-      
+
       nSelected ++;
     }
   }
-  
+
   if (nSelected)
     deleteData (jlist);
 };
-
-
