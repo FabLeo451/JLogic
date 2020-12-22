917b0812a9532540e4e2fc4568b12dd9639bdb6a
FabLeo451 <fab.leo@gmail.com>
2020-12-22 10:47:00 +0100

    Tutorial

---
+++ docs/js/toc.js
@@ -0,0 +1,42 @@
+function buildTOC(container, jsections, prefix, depth) {
+    if (!container)
+        container = document.getElementById("toc");
+
+    if (!depth)
+        depth = 0;
+
+    if (!prefix)
+        prefix = null;
+        
+    prefix = prefix ? prefix : '';
+        
+    t = 1;
+        
+    for (var i=0; i<jsections.length; i++) {
+        elem = document.createElement('div');
+        elem.style.marginLeft = depth + "em";
+        elem.innerHTML = '<a href="'+jsections[i].url+'">'+prefix + t + ' ' + jsections[i].title+'</a>';
+        container.appendChild(elem);
+        
+        if (jsections[i].hasOwnProperty("children")) {
+            buildTOC(container, jsections[i].children, prefix + t + '.', depth+1);
+        }
+        
+        t ++;
+    }
+}
+
+function toc(id) {
+    jexample = [
+        { "title": "Nodes and connectors", "url":"tutorial/nodes.html"},
+        { "title": "Blueprints", "url":"tutorial/blueprints.html", "children": [
+                { "title": "The Main blueprint", "url":"tutorial/blueprints.html#main"},
+                { "title": "The Event blueprint", "url":"tutorial/blueprints.html"}
+            ]
+        },
+        { "title": "Programs", "url":"tutorial/programs.html"},
+        { "title": "Flow control", "url":"tutorial/flow.html"}
+    ];
+    
+    buildTOC(document.getElementById(id ? id : "toc"), jexample);
+}
